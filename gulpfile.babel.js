0f28bf44e3977fd38ccdf2097854464933e236a2
Brady Bryan <jbraden.bryan@gmail.com>
2017-03-26 16:44:59 -0500

    init commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,28 @@
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import babel from 'gulp-babel';
+import babelify from 'babelify';
+import browserify from 'browserify';
+import buffer from 'vinyl-buffer';
+import source from 'vinyl-source-stream';
+import uglify from 'gulp-uglify';
+import stringify from 'stringify';
+
+gulp.task('default', ['bundle']);
+
+gulp.task('bundle', () => {
+    gutil.log('bundle called');
+    const bundler = browserify('src/bookmarklet.js');
+    bundler.transform(babelify);
+    bundler.transform(stringify, {
+      appliesTo: { includeExtensions: ['.html', '.css'] },
+      minify: true
+    });
+
+    bundler.bundle()
+        .on('error', (err) => { console.error(err); })
+        .pipe(source('bookmarklet.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
---
+++ src/bookmarklet.js
@@ -0,0 +1,6 @@
+/*global $:true */
+import testHtml from '../mods/test.html';
+
+$ = window.optimizely.$;
+
+$('body').append(testHtml);
